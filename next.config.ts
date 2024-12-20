557c91d08696eca7a695d2fe9e0c54d2ba811641
Zaker Safdari <zakersafdari@gmail.com>
2024-12-20 21:17:41 +1300

    feat: enhance module resolution and update ESLint configuration

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,22 @@
-import type { NextConfig } from "next";
+import { NextConfig } from "next";
+import { Configuration } from "webpack";
+import path from "path";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  webpack: (config: Configuration) => {
+    config.resolve = {
+      ...config.resolve,
+      alias: {
+        ...config.resolve?.alias,
+        "@components": path.resolve(__dirname, "src/components"),
+        "@db": path.resolve(__dirname, "src/db"),
+        "@lib": path.resolve(__dirname, "src/lib"),
+        "@app": path.resolve(__dirname, "src/app"),
+      },
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
