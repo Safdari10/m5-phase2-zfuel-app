064074a79d38ebad1c0c4345693bc28701718833
Zaker Safdari <zakersafdari@gmail.com>
2025-01-06 16:56:16 +1300

    Added ZFuelStation model with schema for address and prices

---
+++ src/db/model.ts
@@ -0,0 +1,21 @@
+import mongoose from 'mongoose';
+
+const Schema = mongoose.Schema;
+
+const ZFuelStationSchema = new Schema({
+  address: {
+    street: { type: String, required: true },
+    city: { type: String, required: true },
+    state: { type: String, required: true },
+    zip: { type: String, required: true }
+  },
+  prices: {
+    diesel: { type: Number, required: true },
+    fuel91: { type: Number, required: true },
+    fuel95: { type: Number, required: true }
+  }
+});
+
+const ZFuelStation = mongoose.model('ZFuelStation', ZFuelStationSchema);
+
+export default ZFuelStation;
